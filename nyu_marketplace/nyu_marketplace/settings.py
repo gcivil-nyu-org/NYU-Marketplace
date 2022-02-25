7ce42c8e92fc4e8a630386c8952d8113ba2ae407
Shravani Rakshe <Shravani.rakshe@gmail.com>
2022-02-25 15:31:52 -0500

    URLs added

--- nyu_marketplace/nyu_marketplace/settings.py
+++ nyu_marketplace/nyu_marketplace/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'nyu_marketplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['posts/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- nyu_marketplace/posts/urls.py
+++ nyu_marketplace/posts/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
+    path('profile/', views.profile, name='profile'),
+    path('createpost/', views.createpost, name='createpost'),
+    
 ]
