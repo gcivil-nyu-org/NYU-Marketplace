18008ccbc91f54b62b6e859149b33bcd22644e0a
weineng-hsu <weineng.hsu@gmail.com>
2022-03-01 01:11:47 -0500

    Modify createpost.html in line 23 add "" for photo upload Modify views.py for clsss: postCreate(CreateView) Modify home.html for showing pictures in home Modify urls.py for fetching picture

--- nyu_marketplace/posts/views.py
+++ nyu_marketplace/posts/views.py
@@ -1,38 +1,95 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from .forms import PostModelForm
-from django.template import loader
+from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+#from django.template import loader
+
+posts = [
+    {
+
+        'author' : 'Shravani',
+        'college' : 'New York University',
+        'item_type' : 'Sell',
+        'cost' : '$24'
+    },
+    {
+
+        'author' : 'Tanvi',
+        'college' : 'New York University',
+        'item_type' : 'Exchange',
+        'cost' : '$10'
+    },
+     {
+
+        'author' : 'Tanvi',
+        'college' : 'New York University',
+        'item_type' : 'Exchange',
+        'cost' : '$10'
+    },
+]
 
 
-def post_create(request):
-    form = PostModelForm()
-    if request.method == "POST":
-        form = PostModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/posts')
-    context = {
-        "form": form
-    }
-    return render(request, "posts/createpost.html", context)
+def stream_file(request, pk):
+    post = get_object_or_404(Post, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = post.content_type
+    response['Content-Length'] = len(post.picture)
+    response.write(post.picture)
+    return response
+
+
+# def post_create(request):
+#     form = PostModelForm()
+#     if request.method == "POST":
+#         form = PostModelForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('/posts')
+#     context = {
+#         "form": form
+#     }
+#     return render(request, "posts/createpost.html", context)
+
+
+class postCreate(CreateView):
+    success_url = '/posts'
+    template_name = "posts/createpost.html"
+
+    def get(self, request, pk=None):
+        form = PostModelForm()
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        form = PostModelForm(request.POST, request.FILES or None)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        # Add owner to the model before saving
+        post = form.save(commit=False)
+        #ad.owner = self.request.user
+        post.save()
+        form.save_m2m()
+        return redirect(self.success_url)
 
 
 def index(request):
-    latest_posts_list = Post.objects.order_by('-updated_at')[:5]
-    template = loader.get_template('posts/home.html')
+    post_list = Post.objects.all().order_by('-updated_at')[:10]
     context = {
-        'posts': latest_posts_list,
+        'post_list' :  post_list
     }
-    return HttpResponse(template.render(context, request))
+
+    return render(request,'posts/home.html',context)
 
 
 def profile(request):
 
-    # context = {
-    #     'posts' :  posts
-    # }
+    context = {
+        'posts' :  posts
+    }
 
     return render(request,'posts/profile.html')
-
-
