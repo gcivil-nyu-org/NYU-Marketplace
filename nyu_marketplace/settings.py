e1b14829681b0d47ff0eb70f4ed59a5e649db0d5
weineng-hsu <weineng.hsu@gmail.com>
2022-03-09 10:30:33 -0500

    more login: setup email backend and modify some templates for login function

--- nyu_marketplace/settings.py
+++ nyu_marketplace/settings.py
@@ -192,4 +192,12 @@ LOGOUT_REDIRECT_URL = '/accounts/login'
 SIGNUP_REDIRECT_URL = '/posts'
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = None
+ACCOUNT_EMAIL_VERIFICATION = True
+ACCOUNT_EMAIL_SUBJECT_PREFIX = 'NYU Marketplace '
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "nyumarketplace@gmail.com"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
