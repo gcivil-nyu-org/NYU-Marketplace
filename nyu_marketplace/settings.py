50d91067aa5d3de89b69cb528de73d512a191b2b
Shravani Rakshe <Shravani.rakshe@gmail.com>
2022-03-22 22:30:30 -0400

    Changes for Profile Reformat

--- nyu_marketplace/settings.py
+++ nyu_marketplace/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
 
@@ -172,8 +172,8 @@ SITE_ID = 2
 # Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
     "google": {
-        "SCOPE": ["profile", "email", ],
-        "AUTH_PARAMS": {"access_type": "online", },
+        "SCOPE": ["profile", "email",],
+        "AUTH_PARAMS": {"access_type": "online",},
     }
 }
 
