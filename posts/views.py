37df25d9ab5f8fdcf70a13973819e2f126c39c1a
weineng-hsu <weineng.hsu@gmail.com>
2022-03-23 09:32:30 -0400

    fix black8 error

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+
 # from django.http import HttpResponse
 from .models import Post
 from .forms import PostModelForm
