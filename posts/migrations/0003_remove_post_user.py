9192b4b4ead5239b876efc751f748e91933bdac0
furrkai <kz2111@nyu.edu>
2022-03-27 20:47:30 -0400

    Fix black check

--- posts/migrations/0003_remove_post_user.py
+++ posts/migrations/0003_remove_post_user.py
@@ -10,5 +10,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(model_name="post", name="user",),
+        migrations.RemoveField(
+            model_name="post",
+            name="user",
+        ),
     ]
