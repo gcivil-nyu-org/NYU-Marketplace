5cb8e7a08f7a125c3103a831682ddfe70977ef23
furrkai <kz2111@nyu.edu>
2022-03-29 16:52:39 -0400

    Add urls test for profile

--- users/tests/tests_urls.py
+++ users/tests/tests_urls.py
@@ -7,4 +7,4 @@ class TestUrls(SimpleTestCase):
     def test_profile_url_is_resolved(self):
         url = reverse("profile")
         self.assertEquals(resolve(url).func, profile)
-        self.assertEquals(url, "/profile")
+        self.assertEquals(url, "/profile/")
