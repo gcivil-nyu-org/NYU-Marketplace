49f11377738d7455f9c445c898f1d97084e84a2d
weineng-hsu <weineng.hsu@gmail.com>
2022-03-29 21:53:18 -0400

    add some tests for posts view

---
+++ posts/tests/tests_views.py
@@ -0,0 +1,62 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+
+# from posts.models import Post
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        # self.client.login(username='testuser', password='12345')
+        self.user = get_user_model().objects.create_user(
+            username="user", password="12test12", email="user@example.com"
+        )
+        self.poster = get_user_model().objects.create_user(
+            username="test", password="12test12", email="test@example.com"
+        )
+        # post = Post.objects.create(
+        #     name="algorithm textbook",
+        #     description="used testbook for cs6033",
+        #     option="sell",
+        #     category="textbook",
+        #     price=5,
+        #     location="tendon",
+        #     user=self.poster,
+        # )
+        # post2 = Post.objects.create(
+        #     name="macbook pro",
+        #     description="used macbook pro",
+        #     option="exchange",
+        #     category="tech",
+        #     price=50,
+        #     location="stern",
+        #     user=self.poster,
+        # )
+
+    def test_post_get(self):
+        response = self.client.get("/posts/")
+        self.assertEquals(response.status_code, 302)
+        # login = self.client.force_login(self.user)
+        login = self.client.login(username="user", password="12test12")
+        self.assertEquals(login, True)
+
+    def test_post_create_get(self):
+        response = self.client.get("/posts/create/")
+        self.assertEquals(response.status_code, 302)
+        # login = self.client.force_login(self.user)
+        login = self.client.login(username="user", password="12test12")
+        self.assertEquals(login, True)
+        response2 = self.client.get("/posts/create")
+        self.assertEquals(response2.status_code, 301)
+
+    # def test_post_create_create(self):
+    #     newpost = Post.objects.create(
+    #         name="algorithm textbook",
+    #         description="used testbook for cs6033",
+    #         option="sell",
+    #         category="textbook",
+    #         price=5,
+    #         location="tendon",
+    #         user=self.poster,
+    #     )
+    #     response = self.client.post("/posts/create/", )
