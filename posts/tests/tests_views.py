b054c2dc6699d8c73f612d76a32ba7654733d56f
weineng-hsu <weineng.hsu@gmail.com>
2022-04-01 18:34:20 -0400

    added more test for posts/view

--- posts/tests/tests_views.py
+++ posts/tests/tests_views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
-
-# from posts.models import Post
+from posts.models import Post
 
 
 class TestViews(TestCase):
@@ -13,18 +12,15 @@ class TestViews(TestCase):
             password="12test12",
             email="user@nyu.edu",
         )
+        self.admin = get_user_model().objects.create_superuser(
+            username="admin",
+            password="admintestadmin",
+            email="admin@nyu.edu",
+        )
         self.poster = get_user_model().objects.create_user(
             username="test", password="12test12", email="test@example.com"
         )
-        # post = Post.objects.create(
-        #     name="algorithm textbook",
-        #     description="used testbook for cs6033",
-        #     option="sell",
-        #     category="textbook",
-        #     price=5,
-        #     location="tendon",
-        #     user=self.poster,
-        # )
+
         # post2 = Post.objects.create(
         #     name="macbook pro",
         #     description="used macbook pro",
@@ -43,14 +39,22 @@ class TestViews(TestCase):
         self.assertEquals(login, True)
         response2 = self.client.get("/posts/")
         self.assertEquals(response2.status_code, 200)
-        response3 = self.client.get(
-            "/posts/", category="tech", option="rent", sort="priceasc"
-        )
+        data = {"category": "tech", "option": "rent", "sort": "pricedesc"}
+        response3 = self.client.get("/posts/", data)
         self.assertEquals(response3.status_code, 200)
-        response3 = self.client.get(
-            "/posts/", category="tech", option="rent", sort="pricedsc"
-        )
-        self.assertEquals(response3.status_code, 200)
+        data2 = {"category": "tech", "option": "rent", "sort": "priceasc"}
+        response4 = self.client.get("/posts/", data2)
+        self.assertEquals(response4.status_code, 200)
+        data3 = {"category": "tech", "option": "reported", "sort": "priceasc"}
+        response5 = self.client.get("/posts/", data3)
+        self.assertEquals(response5.status_code, 403)
+        self.client.logout()
+        self.assertEquals(login, True)
+        login = self.client.login(email="admin@nyu.edu", password="admintestadmin")
+        self.assertEquals(login, True)
+        data4 = {"category": "tech", "option": "reported", "sort": "priceasc"}
+        response6 = self.client.get("/posts/", data4)
+        self.assertEquals(response6.status_code, 200)
 
     def test_post_create_get(self):
         response = self.client.get("/posts/create/")
@@ -61,3 +65,22 @@ class TestViews(TestCase):
         response2 = self.client.get("/posts/create/")
         self.assertEquals(response2.status_code, 200)
         self.assertTemplateUsed(response2, "posts/createpost.html")
+
+    def test_detail_view(self):
+        response = self.client.get("/posts/detail/1")
+        self.assertEquals(response.status_code, 302)
+        login = self.client.login(email="user@nyu.edu", password="12test12")
+        self.assertEquals(login, True)
+        Post.objects.create(
+            name="macbook pro",
+            description="used macbook pro",
+            option="exchange",
+            category="tech",
+            price=50,
+            location="stern",
+            user=self.poster,
+            picture="https://nyu-marketplace-team1.s3.amazonaws.com/algo.jpg",
+        )
+        response2 = self.client.get("/posts/detail/1")
+        self.assertEquals(response2.status_code, 200)
+        self.assertTemplateUsed(response2, "posts/detail.html")
