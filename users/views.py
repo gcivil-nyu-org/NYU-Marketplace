54daf676fad52a508a905da02c1494353cba951a
nevermoreno1 <yangaodaniel@gmail.com>
2022-04-03 15:36:31 -0400

    # add profile_detail, showing personal info and posts

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import ProfileForm
 from .models import Profile
+from posts.models import Post
 
 # Create your views here.
 # def userTest(request):
@@ -22,4 +23,14 @@ def profile(request):
 
 @login_required(login_url="/accounts/login/")
 def profile_detail(request):
-    return render(request, "users/profile_detail.html")
+    posts = Post.objects.all()
+    posts = posts.filter(user=request.user)
+    user_info = Profile.objects.get(user=request.user)
+    # user_info = user_info.filter(user=request.user)
+
+    context = {
+        "post_list": posts,
+        "user": user_info,
+        "default_user": request.user
+    }
+    return render(request, "users/profile_detail.html", context)
