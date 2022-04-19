00d65090189baadbff215bbd7e66fff36e24ffe6
abirbhav <abirbhavdutta@gmail.com>
2022-04-19 16:04:53 -0400

    Fixed /profile bug

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    # path("", views.profile, name="profile"),
+    path("", views.edit_profile, name="profile"),
     path("profile_detail/", views.profile_detail, name="profile_detail"),
     path("edit_profile/", views.edit_profile, name="edit_profile"),
     path("user_info/<int:user_id>", views.user_info, name="user_info"),
