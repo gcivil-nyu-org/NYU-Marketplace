bb09e0385ee5e2b17fa78dce6d841d5c565e8940
nevermoreno1 <yangaodaniel@gmail.com>
2022-04-25 20:48:12 -0400

    # modify test for price limit

--- posts/tests/tests_forms.py
+++ posts/tests/tests_forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
 from django.test import TestCase
+from io import BytesIO
+from PIL import Image
+from django.core.files.base import File
 
 
 class TestViews(TestCase):
@@ -9,41 +11,50 @@ class TestViews(TestCase):
             username="test", password="12test12", email="test@example.com"
         )
 
+    @staticmethod
+    def get_image_file(name, ext="png", size=(50, 50), color=(256, 0, 0)):
+        file_obj = BytesIO()
+        image = Image.new("RGBA", size=size, color=color)
+        image.save(file_obj, ext)
+        file_obj.seek(0)
+        return File(file_obj, name=name)
+
     def test_price_over_limit(self):
         login = self.client.login(email="test@example.com", password="12test12")
         self.assertEquals(login, True)
+        image1 = self.get_image_file("image.png")
         response = self.client.post(
             "/posts/create/",
             {
                 "name": "macbook pro",
-                "description=": "used macbook pro",
-                "option": "exchange",
+                "description": "used macbook pro",
+                "option": "rent",
                 "category": "tech",
-                "price": "1000000000",
+                "price": 1000000000,
                 "location": "stern",
+                "picture": image1,
                 "user": "self.poster",
-                "picture": "https://nyu-marketplace-team1.s3.amazonaws.com/algo.jpg",
             },
         )
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "error")
-        self.assertRaises(ValidationError)
+        self.assertContains(response, "Price can not be larger than 1000000!")
 
     def test_price_under_limit(self):
         login = self.client.login(email="test@example.com", password="12test12")
         self.assertEquals(login, True)
-        response2 = self.client.post(
+        image1 = self.get_image_file("image.png")
+        response = self.client.post(
             "/posts/create/",
             {
-                "name": "macbook ",
-                "description=": "used macbook pro",
-                "option": "exchange",
+                "name": "macbook pro",
+                "description": "used macbook pro",
+                "option": "rent",
                 "category": "tech",
-                "price": "1000",
+                "price": 10000,
                 "location": "stern",
+                "picture": image1,
                 "user": "self.poster",
-                "picture": "https://nyu-marketplace-team1.s3.amazonaws.com/algo.jpg",
             },
         )
-        self.assertEqual(response2.status_code, 200)
-        self.assertNotContains(response2, "Price can not be larger than 1000000!")
+        self.assertEqual(response.status_code, 302)
