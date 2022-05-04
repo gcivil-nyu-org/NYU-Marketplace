3a5f6f3048b21eff9eeafe8a8ff63c1f8b103b09
weineng-hsu <weineng.hsu@gmail.com>
2022-05-04 13:31:06 -0400

    added 404 page for posts that not exist and added some test

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from .forms import ProfileForm
 from .models import Profile
 from notifications.signals import notify
